231e59a4e253078b326b36d981286bbcba7d9727
Raman Ahuja <iramanahuja94@gmail.com>
2018-09-10 14:21:04 -0700

    Update Question1.java

--- cracking-the-coding-interview/Chapter1-Arrays&Strings/Question1.java
+++ cracking-the-coding-interview/Chapter1-Arrays&Strings/Question1.java
@@ -1,4 +1,6 @@
 
+/* implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+*/
 public class Question1 {
 	public boolean uniqueChars(String str){
 		/*
